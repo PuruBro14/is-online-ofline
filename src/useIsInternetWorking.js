d0d6c8ff5eea44b903cdcd4982f13ad539aef804
PuruBro14 <gokuman0000@gmail.com>
2024-03-14 01:41:27 +0530

    custom-hook

--- src/useIsInternetWorking.js
+++ src/useIsInternetWorking.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
  const useIsInternetWorking = () => {
       const [onlineStatus, setOnlineStatus] = useState(true);
     window.addEventListener("offline", () => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,15 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js", // Entry file of your package
+  entry: "./src/index.js", // Entry file of your project
   output: {
     path: path.resolve(__dirname, "dist"), // Output directory
     filename: "bundle.js", // Output file name
-    libraryTarget: "umd", // Format of the library
-    library: "MyLibrary", // Name of your library
-    umdNamedDefine: true,
   },
   module: {
     rules: [
       {
-        test: /\.js$/, // Apply Babel only to JavaScript files
+        test: /\.(js|jsx)$/, // Apply Babel to JavaScript and JSX files
         exclude: /node_modules/,
         use: {
           loader: "babel-loader", // Use Babel loader
